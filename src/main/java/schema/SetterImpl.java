8fac6d494c943861acd1801667652707ee2fd6bd
Prasanth <kesavarapu.siva@gmail.com>
2019-03-08 20:38:13 +0530

    reformat

--- src/main/java/schema/SetterImpl.java
+++ src/main/java/schema/SetterImpl.java
@@ -1,14 +1,12 @@
 package schema;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collections;
 
 import org.apache.commons.lang.StringUtils;
 
-import static schema.SConstant.separator;
+import static schema.SConstant.*;
 
 public class SetterImpl<Dest> {
     public ArrayList<Method> methods;
@@ -29,13 +27,6 @@ public class SetterImpl<Dest> {
         // Collections.reverse(methods);
     }
 
-    public static String getSetterName(String name) {
-        return "set" + StringUtils.capitalise(name);
-    }
-
-    public static String getGetterName(String name) {
-        return "get" + StringUtils.capitalize(name);
-    }
 
     public void setValue(Dest destObj, Object obj) throws NoSuchMethodException, SecurityException,
             IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchFieldException,
